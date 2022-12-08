ec4223e4798a869a05587615c38c1551172b3a87
Matheus Leal <matheuslealpa@gmail.com>
2022-12-08 13:58:14 -0300

    feat: example lambdas expressions

--- src/JavaLambdaRunneable.java
+++ src/JavaLambdaRunneable.java
@@ -1,2 +1,23 @@
+/**
+ * An interface with only one abstract method is
+ * called a functional interface,
+ * or a Single Abstract Method (SAM) interface.
+ *
+ * The interface may or may not have the
+ * @FunctionalInterface Annotation
+ */
 public class JavaLambdaRunneable {
+    public static void main(String[] args) {
+
+        // Java 7
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world - Java 7");
+            }
+        }).run();
+
+        // Java 8 - Lambdas Expressions
+        new Thread(() -> System.out.println("Hello world - Lambdas Expressions")).run();
+    }
 }
